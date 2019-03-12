e4a37ece14af0630f7b39213fd5bd19c668c5bb1
Miguel Cruz <Miguelc4600@gmail.com>
2019-03-12 01:37:46 -0400

    Reading from a source file and sorting complete

--- src/BackEnd/HTMLParser.java
+++ src/BackEnd/HTMLParser.java
@@ -13,61 +13,80 @@ import java.util.stream.Collectors;
 import java.util.Map.Entry;
 
 public class HTMLParser {
-    String link;
+    static File ctrlFile = new File("control.txt");
+    Scanner sc = new Scanner(ctrlFile);
+    static int amountOfLinks;
 
-    public String grabWebPage(boolean flag) throws IOException {
+
+public static void clearFiles(){
+    File folder = new File("/");
+    File fList[] = folder.listFiles();
+
+    for (File file : fList) {
+        if (file.getName().endsWith(".txt")) {
+            file.delete();
+        }
+        else if(file.getName().startsWith("control.txt")) {
+        }
+//TODO: FIX THIS
+    }
+}
+
+
+    public String grabWebPage(boolean mode) throws IOException {
         // Reads From Control File
-        File file = new File("control.txt");
-        Scanner sc = new Scanner(file);
-        while (sc.hasNextLine()) {
-            this.link = sc.nextLine();
+        clearFiles();
+        String link = "";
+        Document webDoc;
+        Elements webElements;
+        String elements = "";
+
+        mode = false;
+        if(mode){
+            //////
         }
+        for (amountOfLinks = 0 ; amountOfLinks < 10 && sc.hasNextLine(); amountOfLinks++) {
+            System.out.println(amountOfLinks);
+            System.out.println(link);
+            link = sc.nextLine();
 
-        Document webDoc = Jsoup.connect(link).userAgent("Mozilla").data("name", "jsoup").get();
-        Elements webElements = webDoc.select("div#mw-content-text");
-
-
-
-        String[] shortLink  = link.split("^(.*[\\\\\\/])");
-
-        StringBuilder builder = new StringBuilder();
-        for (String value : shortLink) {
-            builder.append(value);
-        }
-        String text = builder.toString();
-        //This Makes the out.file (Makes a output file and cuts off System.out (GUI Depends on this being false))
-
-        if (flag == true) {
+            webDoc = Jsoup.connect(link).userAgent("Mozilla").data("name", "jsoup").get();
+            webElements = webDoc.select("div#mw-content-text");
 
 
-            PrintStream fileOut = new PrintStream(new File (text+".txt"));
+            // To make File titles
+            String[] shortLink = link.split("^(.*[\\\\\\/])");
+
+            StringBuilder builder = new StringBuilder();
+            for (String value : shortLink) {
+                builder.append(value);
+            }
+            String text = builder.toString();
+            //This Makes the output  (Makes a output ctrlFile and cuts off System.out (GUI Depends on this being false))
+            PrintStream fileOut = new PrintStream(new File(text + ".txt"));
+            System.out.println(text);
+
             System.setOut(fileOut);
-        }
+            for (Element el : webElements) {
+                elements = el.text();
 
-        String elements = null;
-        for (Element el : webElements) {
-            elements = el.text();
+            }
+            //--DEBUG
+            //System.out.println(amountOfLinks);
+            // System.out.println(link);
+            printListVertically(filterAndSort(elements));
         }
+        // Parse and returns raw text
+
         return elements;
-    }
+}
 
-
-    public static void printListVertically(List<Entry<String, Long>> listEntry) {
-        for (Entry<String, Long> entry : listEntry) {
-            String key = entry.getKey();
-            Long value = entry.getValue();
-            System.out.println(key + " = " + value);
-        }
-    }
-
-    public static List<Map.Entry<String, Long>> filterAndSort(String string, String regex) {
-
+    public static List<Map.Entry<String, Long>> filterAndSort(String string) {
         //
         Map<String, Long> frequencyMap = Arrays.stream(string.split("\\s+"))
                 .filter(word -> word.matches("\\b\\w{5,}\\b"))
                 .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
         Comparator<Map.Entry<String, Long>> byValue = Comparator.comparing(Map.Entry::getValue);
-
         //
         List<Map.Entry<String, Long>> sortedByFrequency = frequencyMap.entrySet()
                 .stream()
@@ -75,18 +94,20 @@ public class HTMLParser {
                 .collect(Collectors.toList());
 
         return sortedByFrequency;
-
     }
 
-    public void run(String regex) throws IOException {
-        printListVertically(filterAndSort(grabWebPage(true), regex));
-    }
+        public static void printListVertically(List<Entry<String, Long>> listEntry) {
 
+            for (Entry<String, Long> entry : listEntry) {
+                String key = entry.getKey();
+                Long value = entry.getValue();
+                System.out.println(key + " = " + value);
+            }
+
+    }
     // Constructor
     public HTMLParser() throws IOException {
+       grabWebPage(true); // UI supposed to set bool
 
-        run("[^a-zA-Z\\s]+|\\b\\w{0,4}\\b");
-    }
-
-}
+}}
 
